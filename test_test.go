6d37d83e9c117cd46a394886ececa2bc79e2134b
wurui <rui1009479218@gmail.com>
2021-03-22 16:42:01 +0800

    fix: test

--- test_test.go
+++ test_test.go
@@ -10,18 +10,15 @@ type diffpathTest struct {
 	except string
 }
 
-func diffpathTestMockData() []diffpathTest {
-	return []diffpathTest{
-		{a: "/Users/wurui/grafana-backup/1616398799", b: "/Users/wurui/grafana-backup/1616398799/080", except: "/080"},
-		{a: "/Users/wurui/grafana-backup/1616398799", b: "/Users/wurui/grafana-backup/1616398799", except: ""},
-		{a: "", b: "", except: ""},
-		{a: "/Users/wurui/grafana-backup/1616398799", b: "", except: "/Users/wurui/grafana-backup/1616398799"},
-	}
+var mockdata = []diffpathTest{
+	{a: "/Users/wurui/grafana-backup/1616398799", b: "/Users/wurui/grafana-backup/1616398799/080", except: "/080"},
+	{a: "/Users/wurui/grafana-backup/1616398799", b: "/Users/wurui/grafana-backup/1616398799", except: ""},
+	{a: "", b: "", except: ""},
+	{a: "/Users/wurui/grafana-backup/1616398799", b: "", except: "/Users/wurui/grafana-backup/1616398799"},
 }
 
 func Test_diffpath(t *testing.T) {
-	data := diffpathTestMockData()
-	for _, item := range data {
+	for _, item := range mockdata {
 		r := diffpath(item.a, item.b)
 		if r != item.except {
 			t.Errorf("a = %s, b = %s, except = %s, but got = %s", item.a, item.b, item.except, r)
